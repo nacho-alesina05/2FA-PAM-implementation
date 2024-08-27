55140f2ed243c4ddb415b7a49f22438e78c205e2
nacho <ignacio.alesina@fing.edu.uy>
2024-08-27 17:58:49 -0300

    improve code flow

--- main.c
+++ main.c
@@ -109,9 +109,26 @@ char *read_file(const char *file_path) {
 
 int main() {
 
-    
     const char *username = getenv("USER");  // Obtener el nombre de usuario del entorno
     if (!username) {
+        return 1;
+    }
+    // Obtiene el directorio home del usuario actual
+    const char *home_dir = getenv("HOME");
+    if (home_dir == NULL) {
+        return EXIT_FAILURE;
+    }
+    
+    // Construye la ruta del archivo
+    char file_path[256];
+    snprintf(file_path, sizeof(file_path), "%s/2fa.txt", home_dir);
+
+
+    // Verificar si el archivo ya existe
+    FILE *file = fopen(file_path, "r");
+    if (file != NULL) {
+        printf("2fa file does already exists.\n");
+        fclose(file);
         return 1;
     }
     
@@ -138,66 +155,49 @@ int main() {
         }
     }
 
-    // Obtiene el directorio home del usuario actual
-    const char *home_dir = getenv("HOME");
-    if (home_dir == NULL) {
+    // Generar la semilla usando generate_seed
+    unsigned char* secret = generate_seed();
+
+    // Convertir la clave secreta a formato Base32
+    char base32_secret[COTP_SECRET_BASE32_LEN + 1];  // +1 para el carácter nulo
+    custom_base32_encode(secret, COTP_SECRET_MAX_LEN, base32_secret);
+
+    // Abre el archivo en modo escritura, creándolo si no existe
+    file = fopen(file_path, "w");
+    if (file == NULL) {
+        perror("Error al abrir el archivo");
         return EXIT_FAILURE;
     }
 
-    // Construye la ruta del archivo
-    char file_path[256];
-    snprintf(file_path, sizeof(file_path), "%s/2fa.txt", home_dir);
+    // Escribe la semilla en el archivo
+    fprintf(file, "%s\n", aes_encrypt_cbc(base32_secret, password));
+    // Cierra el archivo
+    fclose(file);
 
-
-    // Verificar si el archivo ya existe
-    FILE *file = fopen(file_path, "r");
-    if (file != NULL) {
-        printf("2fa file does already exists.\n");
-        fclose(file);
-    } else {
-        // Generar la semilla usando generate_seed
-        unsigned char* secret = generate_seed();
-
-        // Convertir la clave secreta a formato Base32
-        char base32_secret[COTP_SECRET_BASE32_LEN + 1];  // +1 para el carácter nulo
-        custom_base32_encode(secret, COTP_SECRET_MAX_LEN, base32_secret);
-
-        // Abre el archivo en modo escritura, creándolo si no existe
-        file = fopen(file_path, "w");
-        if (file == NULL) {
-            perror("Error al abrir el archivo");
-            return EXIT_FAILURE;
-        }
-
-        // Escribe la semilla en el archivo
-        fprintf(file, "%s\n", aes_encrypt_cbc(base32_secret, password));
-        // Cierra el archivo
-        fclose(file);
-
-        // Generar el código QR con la URL de configuración de TOTP
-        char url[256];
-        snprintf(url, sizeof(url), "otpauth://totp/%s?secret=%s&issuer=%s", "user", base32_secret, "Example");
-        QRcode *qrcode = QRcode_encodeString(url, 0, QR_ECLEVEL_L, QR_MODE_8, 1);
-        if (!qrcode) {
-            fprintf(stderr, "Error generating QR code.\n");
-            return 1;
-        }
-
-        // Imprimir el QR en ASCII
-        for (int y = 0; y < qrcode->width; y++) {
-            for (int x = 0; x < qrcode->width; x++) {
-                printf(qrcode->data[y * qrcode->width + x] & 1 ? "██" : "  ");
-            }
-            printf("\n");
-        }
-        QRcode_free(qrcode);
-        printf("\n");
-
-        // Cambia los permisos del archivo para que solo el dueño pueda leer y escribir
-        if (chmod(file_path, S_IRUSR | S_IWUSR) != 0) {
-            return EXIT_FAILURE;
-        }
+    // Generar el código QR con la URL de configuración de TOTP
+    char url[256];
+    snprintf(url, sizeof(url), "otpauth://totp/%s?secret=%s&issuer=%s", "user", base32_secret, "Example");
+    QRcode *qrcode = QRcode_encodeString(url, 0, QR_ECLEVEL_L, QR_MODE_8, 1);
+    if (!qrcode) {
+        fprintf(stderr, "Error generating QR code.\n");
+        return 1;
     }
+
+    // Imprimir el QR en ASCII
+    for (int y = 0; y < qrcode->width; y++) {
+        for (int x = 0; x < qrcode->width; x++) {
+            printf(qrcode->data[y * qrcode->width + x] & 1 ? "██" : "  ");
+        }
+        printf("\n");
+    }
+    QRcode_free(qrcode);
+    printf("\n");
+
+    // Cambia los permisos del archivo para que solo el dueño pueda leer y escribir
+    if (chmod(file_path, S_IRUSR | S_IWUSR) != 0) {
+        return EXIT_FAILURE;
+    }
+    
 
     return EXIT_SUCCESS;
 }
